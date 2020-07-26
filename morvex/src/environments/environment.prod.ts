091de8e301633de42cfb7f2d36b1def5fd7e5c0a
Prateek Menon <menon.patrick8@gmail.com>
2020-07-26 18:47:35 -0400

    firebase configuration

--- morvex/src/environments/environment.prod.ts
+++ morvex/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCfgA1Vjg2KDt02AeslBUCf-XLnh95ypiE",
+    authDomain: "morvex-f3973.firebaseapp.com",
+    databaseURL: "https://morvex-f3973.firebaseio.com",
+    projectId: "morvex-f3973",
+    storageBucket: "morvex-f3973.appspot.com",
+    messagingSenderId: "667292773152",
+    appId: "1:667292773152:web:d2b5d8d2951c44747c4545"
+  }
 };
--- morvex/src/environments/environment.ts
+++ morvex/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCfgA1Vjg2KDt02AeslBUCf-XLnh95ypiE",
+    authDomain: "morvex-f3973.firebaseapp.com",
+    databaseURL: "https://morvex-f3973.firebaseio.com",
+    projectId: "morvex-f3973",
+    storageBucket: "morvex-f3973.appspot.com",
+    messagingSenderId: "667292773152",
+    appId: "1:667292773152:web:d2b5d8d2951c44747c4545"
+  }
 };
 
 /*
