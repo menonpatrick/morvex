3a0801603c2e32b8e79d3b6135ba394b4d589400
Prateek Menon <menon.patrick8@gmail.com>
2020-07-28 16:16:28 -0400

    added login with google and logout

--- morvex/src/app/bs-navbar/bs-navbar.component.ts
+++ morvex/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private afAuth: AngularFireAuth ) {
+    afAuth.authState.subscribe((logout) => console.log(logout));
   }
 
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
